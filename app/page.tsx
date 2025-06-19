b590ffeacad60f4780b457f8598f03d0603b43ef
robinjusticeleague <robinjusticeleague@gmail.com>
2025-06-19 11:38:53 +0000

    feat: making the header

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,12 @@
 export default function Home() {
   return (
     <>
-      <header className="border-b h-12 w-full flex items-center justify-start p-1 px-2 gap-1">
+      <header className="border-b h-12 w-full flex items-center justify-start p-1 px-2 gap-2">
         <div className="flex items-center justify-center rounded-md hover:bg-secondary hover:border w-10">
           <svg className="size-8.5" height="1em" xmlns="http://www.w3.org/2000/svg" strokeLinejoin="round" viewBox="0 0 16 16" width="1em"><path clipRule="evenodd" d="M9.50321 5.5H13.2532C13.3123 5.5 13.3704 5.5041 13.4273 5.51203L9.51242 9.42692C9.50424 9.36912 9.5 9.31006 9.5 9.25L9.5 5.5L8 5.5L8 9.25C8 10.7688 9.23122 12 10.75 12H14.5V10.5L10.75 10.5C10.6899 10.5 10.6309 10.4958 10.5731 10.4876L14.4904 6.57028C14.4988 6.62897 14.5032 6.68897 14.5032 6.75V10.5H16.0032V6.75C16.0032 5.23122 14.772 4 13.2532 4H9.50321V5.5ZM0 5V5.00405L5.12525 11.5307C5.74119 12.3151 7.00106 11.8795 7.00106 10.8822V5H5.50106V9.58056L1.90404 5H0Z" fill="black" fillRule="evenodd" /></svg>
         </div>
 
-        <span className="text-2xl h-full flex items-center text-border">/</span>
+        <span className="text-xl h-full flex items-center text-border">/</span>
 
 
 
